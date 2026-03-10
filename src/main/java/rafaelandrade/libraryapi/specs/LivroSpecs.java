0aad95524a381fcd76870e50909b8aa01e9b37e7
Rafael <rn206041@gmail.com>
2026-03-10 13:01:07 -0300

    feat(security): create base security with SecurityFilterChain

---
+++ src/main/java/rafaelandrade/libraryapi/specs/LivroSpecs.java
@@ -0,0 +1,36 @@
+package rafaelandrade.libraryapi.specs;
+
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import org.springframework.data.jpa.domain.Specification;
+import rafaelandrade.libraryapi.model.Genero;
+import rafaelandrade.libraryapi.model.Livro;
+
+public class LivroSpecs {
+
+    public static Specification<Livro> isbnEqual(String isbn){
+        return (root, query, cb) -> cb.equal(root.get("isbn"), isbn);
+    }
+
+    public static Specification<Livro> tituloLike(String titulo){
+        return (root, query, cb) -> cb.like(cb.upper(root.get("titulo")), "%" + titulo.toUpperCase() + "%");
+    }
+
+    public static Specification<Livro> generoEqual(Genero genero){
+        return (root, query, cb) -> cb.equal(root.get("genero"), genero);
+    }
+
+    public static Specification<Livro> anoPublicacaoEqual(Integer anoPublicacao){
+        // and to_char(data_publicacao, 'YYYY') = :anoPublicacao
+        return (root, query, cb) ->
+                cb.equal( cb.function("to_char", String.class,
+                        root.get("dataPublicacao"), cb.literal("YYYY")),anoPublicacao.toString());
+    }
+
+    public static Specification<Livro> nomeAutorLike(String nome){
+        return (root, query, cb) -> {
+            Join<Object, Object> joinAutor = root.join("autor", JoinType.INNER);
+            return cb.like( cb.upper(joinAutor.get("nome")), "%" + nome.toUpperCase() + "%" );
+        };
+    }
+}
