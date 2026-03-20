e388415701a70684ff05816bd389930683fdbcad
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-20 16:14:13 -0300

    docs: configure security scheme of swagger to pass JWT token

--- src/main/java/rafaelandrade/libraryapi/config/OpenApiConfiguration.java
+++ src/main/java/rafaelandrade/libraryapi/config/OpenApiConfiguration.java
@@ -1,8 +1,12 @@
 package rafaelandrade.libraryapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,11 +15,21 @@ import org.springframework.stereotype.Component;
                 title = "Library API",
                 version = "v1",
                 contact = @Contact(
-                        name = "Dougllas Sousa",
-                        email = "dougllas.sousa@libraryapi.com",
+                        name = "Rafael Andrade",
+                        email = "rafaelandrade@libraryapi.com",
                         url = "libraryapi.com"
                 )
-        )
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfiguration {
 }
